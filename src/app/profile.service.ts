b9f04382c57d8c2d52bc5af6877c3301842066e5
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-16 20:57:25 +0300

    create get profile function

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { Repository } from './repository';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  user!:User;
+  repository!:Repository;
 
-  constructor(private http:HttpClient) { }
+
+  constructor(private http:HttpClient) {
+   }
+
+   getProfile(){}
 }
