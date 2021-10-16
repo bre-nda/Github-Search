11445a5331a1c711633939703fa17b220a064921
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-16 23:17:21 +0300

    declare function getProfile

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { Repository } from './repository';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
   private username!: string;
-  private clientid!:'';
-  private clientsecret='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'
 
   constructor(private http:HttpClient) {
+    this.username='bre_nda'
    }
 
    getProfile(){
-
+    return this.http.get('https://api.github.com/users/'+this.username+'?client_id='+environment.clientId + "&client_secret="+environment.clientSecret)
    }
 }
