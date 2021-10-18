bc7f1f2aaf74f5daced7ea80ad215bbe7bd200a6
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-18 12:13:00 +0300

    return api link

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -3,18 +3,38 @@ import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { Repository } from './repository';
 import { environment } from 'src/environments/environment';
+import { CoreEnvironment } from '@angular/compiler/src/compiler_facade_interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  private username!: string;
+  private username: string;
+  user!:any;
+  repos!:any;
 
   constructor(private http:HttpClient) {
-    this.username='bre_nda'
+    console.log("service is now ready")
+    this.username='bre-nda'
    }
 
-   getProfile(){
-    return this.http.get('https://api.github.com/users/'+this.username+'?client_id='+environment.clientId + "&client_secret="+environment.clientSecret)
+  //  getProfile(){
+  //    return this.http.get('https://api.github.com/users/'+this.username+'?client_id='+environment.clientId + "&client_secret="+environment.clientSecret)
+  //  }
+
+  //  getRepos(){
+  //   return this.http.get('https://api.github.com/users/'+this.username+'/repos?order=created&sort=asc?client_id='+environment.clientId + "&client_secret="+environment.clientSecret)
+  //  }
+
+  getProfile(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + environment.clientId + "&client_secret=" + environment.clientSecret)
+  }
+  getRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ environment.clientId + "&client_secret=" + environment.clientSecret)
+  }
+   updateProfile(username:string){
+     this.username = username;
    }
-}
+  }
+
+
