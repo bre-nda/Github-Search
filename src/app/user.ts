8c0a275410205fdb85191eebd0fb57cf2ffbee80
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-18 14:50:06 +0300

    initial commit

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,12 +1,4 @@
 export class User {
-    constructor( public name:string,
-        public followers:number,
-        public following:number,
-        public public_repos:number,
-        public url:string,
-        public avatar_url:string,
-        public login:string){
-
-    }
+   
 }
 
