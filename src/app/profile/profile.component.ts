144614c7eb1cf51d3cc24c8aee1255d6b19967d2
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-18 12:14:16 +0300

    declared function find profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,17 +8,34 @@ import { User } from '../user';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  username!:string;
   profile: any;
   repos:any;
 
+
   constructor(public profileService:ProfileService) { 
-    this.profileService.getProfile().subscribe((profile:any)=>{
-      this.profile = profile
-      console.log(profile)
-    });
+     this.profileService.getProfile().subscribe((profile:any)=>{
+     this.profile = profile
+    console.log(profile)
+     });
     this.profileService.getRepos().subscribe((repos:any)=>{
       this.repos = repos
-    });
+      console.log(repos)
+     });
+  }
+  // searchProfile(){
+  //    this.profileService.updateProfile(this.username);
+  // }
+  findProfile(){
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfile().subscribe((profile:any)=>{
+      this.profile = profile
+     console.log(profile)
+      });
+     this.profileService.getRepos().subscribe((repos:any)=>{
+       this.repos = repos
+       console.log(repos)
+      });
   }
 
   ngOnInit(): void {
