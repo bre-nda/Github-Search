65e3a4e3e40432e9d2721def01c179e79655b5fb
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-18 12:16:30 +0300

    import datecount

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileService } from './profile.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DateCountPipe } from './date-count.pipe';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    DateCountPipe,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
