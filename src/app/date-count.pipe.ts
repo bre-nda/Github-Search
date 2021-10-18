8ed9b469e22c0a253e688f055f29ec52bdb1188d
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-18 12:15:53 +0300

    create pipe

---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var lastSeconds = +todayWithNoTime - +new Date(value)
+    var daysAgo = Math.floor((lastSeconds/3600000)/24)
+    return(daysAgo); daysAgo + " days ago"
+  }
+
+}
